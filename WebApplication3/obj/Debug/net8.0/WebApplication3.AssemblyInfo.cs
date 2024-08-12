b95e5f87fa9f67d104d3f4b352c25caa395b3f87
isikbuse83 <isikbuse83@gmail.com>
2024-08-12 12:55:30 +0300

    at first I tried the ai integration with the api, but then I decided on python as welltest

--- WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb85cf2f04fb0b2f7328a251300e3f255d11525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b8eba4336ebe39ea28c9b1ccaa6705f0c909e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
